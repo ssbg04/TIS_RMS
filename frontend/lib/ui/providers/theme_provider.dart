5f60defa2140f0f9391a253101d8a64bdef04d5d
ssbg04 <crischarlesgarcia345@gmail.com>
2026-08-05 00:45:48 +0800

    change theme mode into light

--- frontend/lib/ui/providers/theme_provider.dart
+++ frontend/lib/ui/providers/theme_provider.dart
@@ -43,12 +43,12 @@ class ThemeModeNotifier extends Notifier<ThemeMode> {
 
   static ThemeMode _fromString(String value) {
     switch (value) {
-      case 'light':
-        return ThemeMode.light;
+      case 'system':
+        return ThemeMode.system;
       case 'dark':
         return ThemeMode.dark;
       default:
-        return ThemeMode.system;
+        return ThemeMode.light;
     }
   }
 }
